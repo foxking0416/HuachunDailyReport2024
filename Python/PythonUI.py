e32a0f7c0fdcbc7f53037624398f9e6955cffe39
WeiChienTu <weichientu@reallusion.com>
2024-04-15 10:33:15 +0800

    更新版UI

--- Python/PythonUI.py
+++ Python/PythonUI.py
@@ -8,41 +8,63 @@ window.title("日報表測試程式")
 window.minsize(width=500, height=500)
 window.resizable(width=True, height=True)
 
-label = Label(text="my label", font=("Arial", 14, "bold"), padx=5, pady=5, bg="red", fg="yellow")
-label.pack()
+def select_start_date():
+    selected_date = calendar_select_start_date.get_date()
+    label_selected_start_date.config(text= selected_date)
 
-def button_clicked():
-    label.config(text="Hello World!")
+def select_current_date():
+    selected_date = calendar_select_current_date.get_date()
+    label_selected_current_date.config(text= selected_date)
 
-button = Button(text="Click Me", font=("Arial", 14, "bold"), padx=5, pady=5, bg="blue", fg="light green", command=button_clicked)
-button.pack()
+group_frame_select_date = Frame(window)
+group_frame_select_date.pack(padx=10, pady=10)
 
-cal = Calendar(window, selectmode='day', date_pattern='yyyy-mm-dd')
-cal.pack(pady=20)
+group_frame_select_start_date = Frame(group_frame_select_date)
+group_frame_select_start_date.pack(side="left", padx=10)
+calendar_select_start_date = Calendar(group_frame_select_start_date, selectmode='day', date_pattern='yyyy-mm-dd')
+calendar_select_start_date.pack(pady=20)
+button_select_start_date = Button(group_frame_select_start_date, text="Select Start Date", command=select_start_date)
+button_select_start_date.pack(pady=10, padx=5)
 
-def select_date():
-    selected_date = cal.get_date()
-    selected_date_label.config(text="Selected date: " + selected_date)
 
-labelStartDate = Label(window, text="開工日期")
-labelStartDate.pack(padx=10, pady=10, anchor="w")
+group_frame_select_current_date = Frame(group_frame_select_date)
+group_frame_select_current_date.pack(side="left", padx=10)
+calendar_select_current_date = Calendar(group_frame_select_current_date, selectmode='day', date_pattern='yyyy-mm-dd')
+calendar_select_current_date.pack(pady=20)
+button_select_current_date = Button(group_frame_select_current_date, text="Select Current Date", command=select_current_date)
+button_select_current_date.pack(pady=10, padx=5)
 
-labelTotalWorkDays = Label(window, text="契約工期")
-labelTotalWorkDays.pack(padx=10, anchor="w")
+group_frame_start_date_label = Frame(window)
+group_frame_start_date_label.pack(anchor="w")
 
-labelExpectTotalCalendarDays = Label(window, text="契約天數")
-labelExpectTotalCalendarDays.pack(padx=10, anchor="w")
+label_start_date = Label(group_frame_start_date_label, text="開工日期")
+label_start_date.pack(side="left",padx=10)
 
 # 顯示選擇的日期
-selected_date_label = Label(window, text="")
-selected_date_label.pack(pady=10)
+label_selected_start_date = Label(group_frame_start_date_label, text="")
+label_selected_start_date.pack(side="left")
 
-# 添加按鈕來獲取選擇的日期
-select_button = Button(window, text="Select Date", command=select_date)
-select_button.pack(pady=10, side="left", padx=5)
 
-cancel_button = Button(window, text="Cancel")
-cancel_button.pack(pady=10, side="left", padx=5)
+group_frame_current_date_label = Frame(window)
+group_frame_current_date_label.pack(anchor="w")
+
+label_current_date = Label(group_frame_current_date_label, text="今日日期")
+label_current_date.pack(side="left",padx=10 )
+
+# 顯示選擇的日期
+label_selected_current_date = Label(group_frame_current_date_label, text="")
+label_selected_current_date.pack(side="left")
+
+
+
+
+
+label_total_workDays = Label(window, text="契約工期")
+label_total_workDays.pack(padx=10, anchor="w")
+
+label_expect_total_calendar_days = Label(window, text="契約天數")
+label_expect_total_calendar_days.pack(padx=10, anchor="w")
+
 
 
 window.mainloop()
