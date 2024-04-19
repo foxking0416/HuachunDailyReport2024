6e01b1a723fd6b1bceac95b973f243e601966ade
WeiChienTu <weichientu@reallusion.com>
2024-04-19 23:12:10 +0800

    新增可以計算幾月幾號是第幾個星期的功能

--- Python/ExportPdf.py
+++ Python/ExportPdf.py
@@ -1,80 +1,7 @@
-# import pandas as pd
-# import matplotlib.pyplot as plt
-
-# # 創建一個示例數據
-# data = {
-#     'Name': ['Alice', 'Bob', 'Charlie', 'David'],
-#     'Age': [25, 30, 35, 40],
-#     'Salary': [50000, 60000, 70000, 80000]
-# }
-
-# # 將數據轉換為 DataFrame
-# df = pd.DataFrame(data)
-
-# # 繪製表格
-# plt.figure(figsize=(8, 4))
-# plt.axis('off')  # 不顯示軸
-# table = plt.table(cellText=df.values, colLabels=df.columns, loc='center')
-
-# # 調整表格的格式
-# table.auto_set_font_size(False)
-# table.set_fontsize(12)
-# table.scale(1.2, 1.2)
-
-# # 將圖表保存為 PDF 檔案
-# plt.savefig('E:\dev\_Python\HuachunDailyReport2024\Python\\table.pdf', bbox_inches='tight')
-# plt.close()
-
-# from reportlab.lib.pagesizes import letter
-# from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Image
-# from reportlab.lib import colors
-
-# def create_pdf_with_table_and_image(pdf_file, table_data, image_file):
-#     doc = SimpleDocTemplate(pdf_file, pagesize=letter)
-    
-#     # 定義表格數據
-#     img = Image('C:\\Users\\WeichienTu\\Desktop\\ScreenShot\\New_ZoomOut.png', width=50, height=50)
-#     table_data_with_image = [['', '', img],
-#                              ['Value 1', 'Value 2', '']]
-#     data = table_data_with_image
-    
-#     # 定義表格樣式
-#     style = TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.grey),
-#                         ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
-#                         ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-#                         ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-#                         ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-#                         ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
-#                         ('GRID', (0, 0), (-1, -1), 1, colors.black)])
-    
-#     # 創建表格對象
-#     table = Table(data)
-    
-#     # 設置表格樣式
-#     table.setStyle(style)
-    
-#     # 將圖片添加到表格中
-
-    
-#     # 將表格數據添加到文檔中
-#     doc.build([table])
-
-# # 指定 PDF 文件的路径
-# pdf_file = "E:\\dev\\_Python\\HuachunDailyReport2024\\Python\\table.pdf"
-
-# # 指定表格数据
-# table_data = [
-#     ['Column 1', 'Column 2', 'Column 3'],
-#     ['Value 1', 'Value 2', 'Value 3'],
-#     ['Value 4', 'Value 5', 'Value 6']
-# ]
-
-# # 指定图像文件的路径
-# image_file = "example.png"
-
-# # 创建包含表格和图像的 PDF
-# create_pdf_with_table_and_image(pdf_file, table_data, image_file)
-
+import json
+import os
+from datetime import datetime
+import ScheduleCount
 import openpyxl
 from openpyxl.drawing.image import Image
 from openpyxl.utils import get_column_letter
@@ -88,6 +15,9 @@ from openpyxl.drawing.spreadsheet_drawing import AbsoluteAnchor
 from openpyxl.drawing.xdr import XDRPoint2D, XDRPositiveSize2D
 from openpyxl.utils.units import pixels_to_EMU, cm_to_EMU
 from openpyxl.drawing.spreadsheet_drawing import OneCellAnchor, AnchorMarker
+
+import unittest
+
 
 def ReadCellDimension( worksheet ):
     column_widthA = worksheet.column_dimensions["A"].width
@@ -143,11 +73,100 @@ def InsertImageIntoExcel(excel_file, image_path, output_excel):
 
     pass
 
+def read_data_and_export_file():
+    arrGlobalConstHoliday = []
+    arrGlobalConstWorkday = []
+    dictGlobalWeatherRelatedHoliday = {}
+    dictGlobalExtendData = {}
+    ScheduleCount.LoadJsonHolidayData(arrGlobalConstHoliday,arrGlobalConstWorkday)
+
+    current_dir = os.path.dirname(__file__)
+    json_file_path = os.path.join(current_dir, 'DailyReport.json')
+
+    with open(json_file_path,'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    for item in data:
+        date_obj = datetime.strptime(item["date"], "%Y-%m-%d")
+        year = date_obj.year
+        month = date_obj.month
+        day = date_obj.day
+
+        weekday = date_obj.weekday()
+
+        first_day_per_month = datetime(year, month, day)
+
+
+        # nWeekday = kExpectEndDate.weekday()
+
+        if item["morning_weather"] == 0:
+            if item["afternoon_weather"] == 0:
+                pass
+            else:
+                pass
+            pass
+        else:
+            if item["afternoon_weather"] == 0:
+                pass
+            else:
+                pass
+    pass
+
+def test_week_num( date ):
+    date_obj = datetime.strptime(date, "%Y-%m-%d")
+    day = date_obj.day
+
+    weekday = ( date_obj.weekday() + 2 ) % 7 
+    if weekday == 0:
+        weekday += 7
+    week_num = (day - 1) // 7 + 1
+    rest = day % 7
+    if rest == 0:
+        rest += 7
+    if weekday - rest < 0:
+        week_num += 1
+    
+    return week_num
 
 # Example usage
-excel_file = 'C:\\_Everything\\HuachunDailyReport2024\\Python\\test.xlsx'
-image_path_sun_all = 'C:\\_Everything\\HuachunDailyReport2024\\Python\\Sun_All.png'
-output_excel = 'C:\\_Everything\\HuachunDailyReport2024\\Python\\testOutput.xlsx'
-pdf_file = 'C:\\_Everything\\HuachunDailyReport2024\\Python\\output.pdf'
+# excel_file = 'C:\\_Everything\\HuachunDailyReport2024\\Python\\test.xlsx'
+# image_path_sun_all = 'C:\\_Everything\\HuachunDailyReport2024\\Python\\Sun_All.png'
+# image_path_rain_all = 'C:\\_Everything\\HuachunDailyReport2024\\Python\\Rain_All.png'
 
-InsertImageIntoExcel(excel_file, image_path_sun_all, output_excel)
+
+
+# output_excel = 'C:\\_Everything\\HuachunDailyReport2024\\Python\\testOutput.xlsx'
+# pdf_file = 'C:\\_Everything\\HuachunDailyReport2024\\Python\\output.pdf'
+# InsertImageIntoExcel(excel_file, image_path_sun_all, output_excel)
+
+# read_data_and_export_file()
+
+# test_week_num('2024-03-10')
+
+class TestFunction(unittest.TestCase):
+    def test_week_number_1(self):
+        returnValue = test_week_num('2024-02-14')
+        self.assertEqual(returnValue, 3)
+        returnValue = test_week_num('2024-03-09')
+        self.assertEqual(returnValue, 2)
+        returnValue = test_week_num('2024-03-10')
+        self.assertEqual(returnValue, 3)
+        returnValue = test_week_num('2024-03-31')
+        self.assertEqual(returnValue, 6)
+        returnValue = test_week_num('2024-04-06')
+        self.assertEqual(returnValue, 1)
+        returnValue = test_week_num('2024-04-07')
+        self.assertEqual(returnValue, 2)
+        returnValue = test_week_num('2024-06-30')
+        self.assertEqual(returnValue, 6)
+        returnValue = test_week_num('2024-09-07')
+        self.assertEqual(returnValue, 1)
+        returnValue = test_week_num('2024-09-14')
+        self.assertEqual(returnValue, 2)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
